a408f18ffa82b288f3d3771a94c64f9193d01268
cvaughn42 <cvaughn42@comcast.net>
2016-09-06 16:38:50 -0400

    Added tests

--- test/Stack.js
+++ test/Stack.js
@@ -1,26 +1,12 @@
-var should = require('chai').should();
 var stack = require('../Stack');
+var assert = require('assert');
 
-describe('#push', function() {
-  
-    it('pushes "test" onto stack', function() {
-        stack.push('test');
-        stack.pop().should.equal('test');
+describe('Testing Stack', function() {
+
+    it('We can push to an empty stack', function() {
+
+        stack.push(3);
+        assert(stack.values[0] === 3);
     });
-});
 
-describe('#pop', function() {
-
-    it('pops "test" from stack', function() {
-        stack.push('test');
-        stack.pop().should.equal('test');
-    });
-});
-
-describe('#peek', function() {
-
-    it('peek "test" from stack', function() {
-        stack.push('test');
-        stack.peek().should.equal('test');
-    });
 });
