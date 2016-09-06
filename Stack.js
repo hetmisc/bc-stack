096ac11fc05911586999671445b763d5b61679ea
cvaughn42 <cvaughn42@comcast.net>
2016-09-06 14:55:07 -0400

    Created initially

---
+++ Stack.js
@@ -0,0 +1,27 @@
+/**
+ * Represents a stack (classic object)
+ */
+function Stack()
+{
+    this.values = [];
+}
+
+Stack.prototype.push = function(value) {
+    this.values.push(value);
+};
+
+Stack.prototype.pop = function() {
+    return this.values.pop();
+};
+
+Stack.prototype.peek = function() {
+
+    if (this.values.length)
+    {
+        return this.values[this.values.length - 1];
+    }
+    else
+    {
+        return null;
+    }
+};
