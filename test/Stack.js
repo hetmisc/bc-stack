8f3212f1b4466ac300f9b90e06cf28696c3e0838
cvaughn42 <cvaughn42@comcast.net>
2016-09-07 08:45:02 -0400

    Added tests

--- test/Stack.js
+++ test/Stack.js
@@ -4,9 +4,23 @@ var assert = require('assert');
 describe('Testing Stack', function() {
 
     it('We can push to an empty stack', function() {
-
+        stack.values = [];
         stack.push(3);
         assert(stack.values[0] === 3);
+    });
+
+    it('Gets "undefined" when poping from an empty stack', function() {
+        stack.values = [];
+        var result = stack.pop();
+        assert(result === undefined);
+    });
+
+    it('Peek returns last element in stack without removing it', function() {
+        var testValue = "Test Value";       
+        stack.values = [testValue];
+        var result = stack.peek();
+        assert(result === testValue);
+        assert(stack.values[0] === testValue);
     });
 
 });
